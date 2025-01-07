f532f7cded9295cd5022ede4338eacf041b1197b
mahafizurr <mahafizurgub@gmail.com>
2025-01-08 01:14:18 +0600

    my initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,35 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCache from "@emotion/cache";
 
-export default function Document() {
+// Create an emotion cache
+const emotionCache = createCache({ key: "css", prepend: true });
+const { extractCritical } = createEmotionServer(emotionCache);
+
+export default function Document({ emotionStyleTags }) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>{emotionStyleTags}</Head>
       <body>
+        <ColorModeScript />
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
+Document.getInitialProps = async (ctx) => {
+  const initialProps = await ctx.defaultGetInitialProps(ctx);
+  const styles = extractCritical(initialProps.html);
+  return {
+    ...initialProps,
+    emotionStyleTags: (
+      <style
+        data-emotion={`${styles.ids.join(" ")}`}
+        dangerouslySetInnerHTML={{ __html: styles.css }}
+      />
+    ),
+  };
+};
